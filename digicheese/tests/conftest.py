b2daa927372c71a66e55317f02b45448d6a306fe
victor odin <victor.odin@sereema.com>
2026-01-19 15:17:58 +0100

    feat: logged_in fixtures

--- digicheese/tests/conftest.py
+++ digicheese/tests/conftest.py
@@ -1,4 +1,8 @@
 import pytest
+
+from digicheese.enums.role_enum import RoleEnum
+from digicheese.models.role import Role
+from digicheese.models.roles_utilisateur import RolesUtilisateur
 from .. import create_app
 from .. import db
 from digicheese.models import Utilisateur as User
@@ -9,13 +13,29 @@ def app():
     with app.app_context():
         db.create_all()
 
-        user = User(name='admin', email='admin@test.com')
-        user.set_password('admin123')
-        db.session.add(user)
+        admin = User(name='admin', email='admin@test.com')
+        admin.set_password('admin123')
+        colis = User(name='colis', email='colis@test.com')
+        colis.set_password('colis123')
+        db.session.add(admin)
+        db.session.add(colis)
+
+        role = Role(libelle=RoleEnum.ADMIN.value)
+        role1 = Role(libelle=RoleEnum.COLIS.value)
+        role2 = Role(libelle=RoleEnum.STOCK.value)
+        db.session.add(role)
+        db.session.add(role1)
+        db.session.add(role2)
+
+        role_utilisateur_admin = RolesUtilisateur(user=admin, role=role)
+        role_utilisateur_colis = RolesUtilisateur(user=colis, role=role1)
+        db.session.add(role_utilisateur_admin)
+        db.session.add(role_utilisateur_colis)
+        
         db.session.commit()
 
-
         yield app
+
         db.session.remove()
         db.drop_all()
 
@@ -24,22 +44,23 @@ def app():
 def client(app):
     return app.test_client()
 
-
-# @pytest.fixture
-# def create_admin_user(app):
-#     with app.app_context():
-#         user = User(name='admin', email='admin@test.com')
-#         user.set_password('admin123')
-#         db.session.add(user)
-#         db.session.commit()
-#         return user
-
+@pytest.fixture
+def authenticated_admin_client(client):
+    with client:
+        client.post(
+            "/api/login",
+            headers={"Content-Type": "application/json"},
+            json={"email": "admin@test.com", "password": "admin123"}
+        )
+        yield client
 
 @pytest.fixture
-def regular_user(app):
-    with app.app_context():
-        user = User(name='user', email='user@test.com')
-        user.set_password('user123')
-        db.session.add(user)
-        db.session.commit()
-        return user
+def authenticated_package_client(client):
+    """Client authenticated as a regular user"""
+    with client:
+        client.post(
+            "/api/login",
+            headers={"Content-Type": "application/json"},
+            json={"email": "colis@test.com", "password": "colis123"}
+        )
+        yield client
--- digicheese/tests/test_admin.py
+++ digicheese/tests/test_admin.py
@@ -1,73 +1,35 @@
 from pprint import pprint
-from .conftest import client, regular_user
 from digicheese.models import Utilisateur as User
 
 
 class TestAdminRoutes:
     def test_redirect_to_login(self, client):
         response = client.get("/admin/users/", follow_redirects=True)
-        pprint(vars(response.request))
+
         # Check redirect to login page when not logged in
         assert "?next=/admin/users/" in response.request.url
         assert response.status_code == 200
 
-
-    def test_login_json(self, client):
+    def test_authentication_json(self, client):
         headers = {"Content-Type": "application/json"}
-        response = client.post(
-            "/api/login", 
-            headers=headers, 
-            json={"email": "admin@test.com", "password": "admin123"}
-        )
+        with client:
+            response = client.post(
+                "/api/login", 
+                headers=headers, 
+                json={"email": "admin@test.com", "password": "admin123"}
+            )
         assert response.json.get("id") == 1
         assert response.json.get("name") == "admin"
         assert response.json.get("email") == "admin@test.com"
         assert response.status_code == 200
+    
+    def test_page_needing_login(self, authenticated_admin_client):
+        response = authenticated_admin_client.get("/profile")
+        assert "Welcome, admin" in response.get_data(as_text=True)
 
-
-def test_modify_session(client):
-    with client.session_transaction() as session:
-        # set a user id without going through the login route
-        session["user_id"] = 1
-
-    # session is saved now
-
-    response = client.get("/users/me")
-    assert response.json["username"] == "flask"
+    
 
 
 
-    # def test_admin_dashboard_requires_admin(client, regular_user):
-    #     client.post("/login", data={"username": "user", "password": "user123"})
-    #     response = client.get("/admin/")
-    #     assert response.status_code == 403
 
 
-    # def test_admin_dashboard_access(client, admin_user):
-    #     client.post("/login", data={"username": "admin", "password": "admin123"})
-    #     response = client.get("/admin/")
-    #     assert response.status_code == 200
-
-
-    # def test_admin_users_list(client, admin_user):
-    #     client.post("/login", data={"username": "admin", "password": "admin123"})
-    #     response = client.get("/admin/users")
-    #     assert response.status_code == 200
-
-
-    # def test_admin_delete_user(client, admin_user, regular_user):
-    #     client.post("/login", data={"username": "admin", "password": "admin123"})
-    #     response = client.post(f"/admin/users/{regular_user.id}/delete")
-    #     assert response.status_code in [200, 302]
-
-
-    # def test_admin_cheeses_list(client, admin_user):
-    #     client.post("/login", data={"username": "admin", "password": "admin123"})
-    #     response = client.get("/admin/cheeses")
-    #     assert response.status_code == 200
-
-
-    # def test_admin_orders_list(client, admin_user):
-    #     client.post("/login", data={"username": "admin", "password": "admin123"})
-    #     response = client.get("/admin/orders")
-    #     assert response.status_code == 200
