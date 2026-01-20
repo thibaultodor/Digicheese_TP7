68fd05f726218bc16342ff26b4bac3f0a356776b
victor odin <victor.odin@sereema.com>
2026-01-20 11:09:09 +0100

    fix: remove redirect to login

--- digicheese/__init__.py
+++ digicheese/__init__.py
@@ -18,7 +18,7 @@ def create_app():
 
     # Configure Flask-Login
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    # login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     # Register blueprints
